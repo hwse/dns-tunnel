4668c5cd8901db60fd585d78d68af5b8f0a5003b
hwse <hagen.weisse@stud.htwk-leipzig.de>
2020-06-10 22:20:44 +0200

    stuff

--- dns-tunnel-client/src/main.rs
+++ dns-tunnel-client/src/main.rs
@@ -1,3 +1,9 @@
+use std::net::UdpSocket;
+use dns_tunnel_lib::header::Header;
+
 fn main() {
-    println!("Hello, world!");
+    let socket = UdpSocket::bind("0.0.0.0:5353").expect("Cant bind port");
+    let message: [u8;5] = [1,2,3,4,5];
+    let message_2: Vec<u8> = vec![5,1,1,1];
+    let bytes_sent = socket.send_to(&message_2[..], "127.0.0.1:53").unwrap();
 }
